5167b7f5fe06a825a9a9ca337c756f22217bc189
suarez9093 <suarez9093@hotmail.com>
2021-04-07 12:51:41 -0400

    installed packages. Working on header

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+function Header() {
+  return <header>Header</header>;
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+  color: 'red';
+`;
