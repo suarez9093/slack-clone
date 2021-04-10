87ddb3ac979a2aa3d05cae3c225f4edbfbd2516b
suarez9093 <suarez9093@hotmail.com>
2021-04-10 14:29:12 -0400

    header done. Working on the sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import AppBody from './components/AppBody';
 import Header from './components/Header';
 function App() {
   return (
     <div className='App'>
       <Header />
+      <AppBody>
+        <Switch>
+          <Route exact path='/'>
+            {/* Chat */}
+          </Route>
+        </Switch>
+      </AppBody>
     </div>
   );
 }
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { SidebarContainer } from './sidebar.style';
+function Sidebar() {
+  return <SidebarContainer></SidebarContainer>;
+}
+
+export default Sidebar;
